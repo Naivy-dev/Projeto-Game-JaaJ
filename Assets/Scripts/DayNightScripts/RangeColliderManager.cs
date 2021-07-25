000bf9f3d6f14e62bbaa2ba5a88ffc9030287532
Foot-G <gustavoplm@hotmail.com>
2021-07-25 16:24:32 -0300

    Mêcanica do isCollidable funcional

--- Assets/Scripts/DayNightScripts/RangeColliderManager.cs
+++ Assets/Scripts/DayNightScripts/RangeColliderManager.cs
@@ -7,21 +7,22 @@ public class RangeColliderManager : MonoBehaviour
     [SerializeField] private RangeColliderPreset Preset;
     [SerializeField] private LightingManager Lighting;
 
+    public GameObject Cube;
+
     private void Update()
     {
         Debug.Log(Lighting.TimeOfDay);
         Debug.Log(Preset.minTime);
         Debug.Log(Preset.maxTime);
 
-        if (Preset.minTime < Lighting.TimeOfDay)
+        if (Preset.minTime < Lighting.TimeOfDay && Preset.maxTime > Lighting.TimeOfDay)
         {
-            if(Preset.maxTime > Lighting.TimeOfDay)
-            {
-                Preset.isCollidable = true;
-            }
+            Preset.isCollidable = true;
+            //Cube.transform.position = new Vector3(Cube.transform.position.x,0.5f,Cube.transform.position.z);
         }
         else
         {
+            //Cube.transform.position = new Vector3(Cube.transform.position.x, -0.6f, Cube.transform.position.z);
             Preset.isCollidable = false;
         }
     }
