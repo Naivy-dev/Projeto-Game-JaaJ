9c196df1e3e8f0ec4ba37e21b552cc1a0ad4ead9
Naivy-dev <arth.brites@gmail.com>
2021-07-25 14:43:45 -0300

    Bug Fix + Day/Night Cycle
    Resolvemos o bug da sensibilidade do mouse depender do fps e adicionamos o ciclo de dia e noite.

---
+++ Assets/Scripts/DayNightScripts/LightingPreset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Lighting Preset", menuName ="ScriptablesObj/Lighting Preset")]
+public class LightingPreset : ScriptableObject
+{
+    public Gradient AmbientColor;
+    public Gradient DirectionalColor;
+    public Gradient FogColor;
+
+   
+}
