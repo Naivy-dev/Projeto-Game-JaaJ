d8eac574d75da23c6fb789204c7e393fb4c1067c
Foot-G <gustavoplm@hotmail.com>
2021-07-30 22:26:15 -0300

    Collider de respawn arrumado

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReturnToStart : MonoBehaviour
+public class RespawnManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public CharacterController Player;
